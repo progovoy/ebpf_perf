87a61f431379cf1ce997bee221df91ddcaa00641
Pavel Rogovoy <p.rogovoy@gmail.com>
2019-03-23 21:17:32 +0200

    make the user to kernel params work

--- metrics/per_core_softirq_count.py
+++ metrics/per_core_softirq_count.py
@@ -3,6 +3,8 @@
 # @lint-avoid-python-3-compatibility-imports
 
 from __future__ import print_function
+
+import time
 from bcc import BPF
 import argparse
 
@@ -55,7 +57,12 @@ typedef struct irq_key_cpu {
     u32 cpu;
 } irq_key_cpu_t;
 
+typedef struct user_args_t {
+    u32 limit;
+} user_args_t;
+
 BPF_HISTOGRAM(dist_cpu, irq_key_cpu_t);
+BPF_ARRAY(user_args, user_args_t, 1);
 
 TRACEPOINT_PROBE(irq, softirq_exit)
 {
@@ -63,18 +70,23 @@ TRACEPOINT_PROBE(irq, softirq_exit)
     cpu_key.vec = args->vec;
     cpu_key.cpu = bpf_get_smp_processor_id();
     dist_cpu.increment(cpu_key);
+    u64* user_val;
     
     u64* val = dist_cpu.lookup(&cpu_key);
     if (!val)
         return 0;
         
-    if (*val > ALERT_LIMIT)
+    int zero = 0;
+    user_args_t* uargs = user_args.lookup(&zero);
+    if (!uargs)
+        return 0;
+        
+    if (*val > uargs->limit)
     {
         alert_t alert;
         alert.timestamp = bpf_ktime_get_ns();
-        
+
         alerts.perf_submit(args, &alert, sizeof(alert_t));
-        
         dist_cpu.delete(&cpu_key);
     }
     
@@ -84,9 +96,17 @@ TRACEPOINT_PROBE(irq, softirq_exit)
 
 b = BPF(text=bpf_text)
 
+def update_prog_args(limit):
+    prog_args = b['user_args']
+    args_val = prog_args[0]
+    args_val.limit = limit
+    prog_args.update({0: args_val})
+
+
 def handle_alert(cpu, data, size):
     alert = b['alerts'].event(data)
-    print('too many interrupts on CPU #{} at time {}!'.format(cpu, alert.timestamp))
+    print('.', end='')
+    #print('too many interrupts on CPU #{} at time {}!'.format(cpu, alert.timestamp))
 
 def vec_to_name(vec):
     # copied from softirq_to_name() in kernel/softirq.c
@@ -94,28 +114,29 @@ def vec_to_name(vec):
     return ["hi", "timer", "net_tx", "net_rx", "block", "irq_poll",
             "tasklet", "sched", "hrtimer", "rcu"][vec]
 
+update_prog_args(100)
+
 b['alerts'].open_perf_buffer(handle_alert)
 
-print("Tracing soft irq event time... Hit Ctrl-C to end.")
-
-# output
 exiting = 0 if args.interval else 1
 dist_cpu = b.get_table("dist_cpu")
+
+args_test = [5, 100]
+i = 0
+
 while (1):
     try:
-        b.kprobe_poll()
+        t_end = time.time() + 3
+        while time.time() < t_end:
+            b.perf_buffer_poll()
+
+        print("updating with {}".format(args_test[i]))
+        update_prog_args(args_test[i])
+        i += 1
+        i = i % len(args_test)
     except KeyboardInterrupt:
         exiting = 1
 
-    # print(dir(dist_cpu))
-    #
-    # for k,v in dist_cpu.items():
-    #     pass
-    #
-    # dist_cpu.print_linear_hist(section_print_fn=vec_to_name)
-    #
-    # dist_cpu.clear()
-    #
     countdown -= 1
     if exiting or countdown == 0:
         exit()
