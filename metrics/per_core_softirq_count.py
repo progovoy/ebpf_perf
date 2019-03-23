5158b24758093b2ff1d7c57d3d2dd04d7645f25a
Pavel Rogovoy <p.rogovoy@gmail.com>
2019-03-23 19:33:35 +0200

    alerts

--- metrics/per_core_softirq_count.py
+++ metrics/per_core_softirq_count.py
@@ -4,7 +4,6 @@
 
 from __future__ import print_function
 from bcc import BPF
-from time import sleep, strftime
 import argparse
 
 # arguments
@@ -43,6 +42,14 @@ debug = 0
 bpf_text = """
 #include <uapi/linux/ptrace.h>
 
+#define ALERT_LIMIT 100
+
+typedef struct alert {
+    u64 timestamp;
+} alert_t;
+
+BPF_PERF_OUTPUT(alerts);
+
 typedef struct irq_key_cpu {
     u32 vec;
     u32 cpu;
@@ -53,31 +60,41 @@ BPF_HISTOGRAM(dist_cpu, irq_key_cpu_t);
 TRACEPOINT_PROBE(irq, softirq_exit)
 {
     irq_key_cpu_t cpu_key = {0};
-
-    // store
     cpu_key.vec = args->vec;
     cpu_key.cpu = bpf_get_smp_processor_id();
     dist_cpu.increment(cpu_key);
-
-    // store as sum or histogram
+    
+    u64* val = dist_cpu.lookup(&cpu_key);
+    if (!val)
+        return 0;
+        
+    if (*val > ALERT_LIMIT)
+    {
+        alert_t alert;
+        alert.timestamp = bpf_ktime_get_ns();
+        
+        alerts.perf_submit(args, &alert, sizeof(alert_t));
+        
+        dist_cpu.delete(&cpu_key);
+    }
     
     return 0;
 }
 """
 
-if debug or args.ebpf:
-    print(bpf_text)
-    if args.ebpf:
-        exit()
-
-# load BPF program
 b = BPF(text=bpf_text)
+
+def handle_alert(cpu, data, size):
+    alert = b['alerts'].event(data)
+    print('too many interrupts on CPU #{} at time {}!'.format(cpu, alert.timestamp))
 
 def vec_to_name(vec):
     # copied from softirq_to_name() in kernel/softirq.c
     # may need updates if new softirq handlers are added
     return ["hi", "timer", "net_tx", "net_rx", "block", "irq_poll",
             "tasklet", "sched", "hrtimer", "rcu"][vec]
+
+b['alerts'].open_perf_buffer(handle_alert)
 
 print("Tracing soft irq event time... Hit Ctrl-C to end.")
 
@@ -86,19 +103,19 @@ exiting = 0 if args.interval else 1
 dist_cpu = b.get_table("dist_cpu")
 while (1):
     try:
-        sleep(int(args.interval))
+        b.kprobe_poll()
     except KeyboardInterrupt:
         exiting = 1
 
-    print(dir(dist_cpu))
-
-    for k,v in dist_cpu.items():
-        pass
-
-    dist_cpu.print_linear_hist(section_print_fn=vec_to_name)
-
-    dist_cpu.clear()
-
+    # print(dir(dist_cpu))
+    #
+    # for k,v in dist_cpu.items():
+    #     pass
+    #
+    # dist_cpu.print_linear_hist(section_print_fn=vec_to_name)
+    #
+    # dist_cpu.clear()
+    #
     countdown -= 1
     if exiting or countdown == 0:
         exit()
