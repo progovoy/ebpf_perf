502c4776661591fae4708fbfa155cbbe57bb1e72
Ido Ben-Yair <ido.benyair@gmail.com>
2019-04-26 16:39:42 +0300

    Made things work on VM again

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,11 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/bionic64"
   config.vm.box_check_update = false
+
   config.vm.synced_folder "..", "/project"
+
+  config.vm.network "forwarded_port", guest: 9090, host: 9090
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+
   config.vm.provision "shell", path: "../scripts/provision.sh"
 end
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y python3.7 python3-venv python3-pip bpfcc-tools linux-headers-$(uname -r) python3-bpfcc
+apt-get install -y python3.7 python3-venv python3-pip bpfcc-tools linux-headers-$(uname -r) python3-bpfcc docker.io
 
-pip3 install pipenv
+pip3 install pipenv docker-compose
 cd /project
 pipenv --python 3.7
 pipenv install
 
 cp  -r /usr/lib/python3/dist-packages/bcc/ $(pipenv --venv)/lib/python3.7/
+
+docker pull prom/prometheus
+docker pull grafana/grafana
+
+mkdir -p storage/grafana
+touch storage/grafana/grafana.db
+chmod -R 0777 storage
