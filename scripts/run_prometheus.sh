81abab9f66c8986f3b7524f72690aae93d5d7641
Pavel Rogovoy <p.rogovoy@gmail.com>
2019-04-26 13:38:13 +0300

    fix prometheus run script

--- scripts/run_prometheus.sh
+++ scripts/run_prometheus.sh
@@ -4,20 +4,22 @@ CUR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 echo "Running: docker rm prometheus
 docker run --name prometheus -d --network host -v \
-${CUR_DIR}/config/prometheus.yml:/etc/prometheus/prometheus.yml \
+${CUR_DIR}/../config/prometheus.yml:/etc/prometheus/prometheus.yml \
 prom/prometheus" 
-docker rm prometheus
+docker rm -f prometheus
 docker run --name prometheus -d --network host -v \
-${CUR_DIR}/config/prometheus.yml:/etc/prometheus/prometheus.yml \
+${CUR_DIR}/../config/prometheus.yml:/etc/prometheus/prometheus.yml \
 prom/prometheus
 
 echo "Running: docker rm grafana
 mkdir /tmp/storage
+chmod -R 0777 /tmp/storage
 docker run --network host -d --name grafana -v \
-${CUR_DIR}/config/grafana.ini:/etc/grafana/grafana.ini -v \
+${CUR_DIR}/../config/grafana.ini:/etc/grafana/grafana.ini -v \
 /tmp/storage:/var/lib/grafana grafana/grafana"
-docker rm grafana
-mkdir /tmp/storage
+docker rm -f grafana
+sudo mkdir /tmp/storage
+sudo chmod -R 0777 /tmp/storage
 docker run --network host -d --name grafana -v \
-${CUR_DIR}/config/grafana.ini:/etc/grafana/grafana.ini -v \
+${CUR_DIR}/../config/grafana.ini:/etc/grafana/grafana.ini -v \
 /tmp/storage:/var/lib/grafana grafana/grafana
