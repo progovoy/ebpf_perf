bug fixes, added mean stat calculation

metrics/metric.py
@@ -1,8 +1,11 @@
 import itertools
+import logging
 from dataclasses import dataclass
-from typing import Iterable, Callable, Any
+from typing import Iterable, Any, Callable
 
 import numpy as np
+
+LOGGER = logging.getLogger()
 
 
 @dataclass()
@@ -15,25 +18,43 @@ class Dim(object):
 class Metric(object):
     name: str
     shape: Iterable[int]
-    collection_algorithm: Callable
+    collector: Any
+    stats: Iterable[Callable] = None
 
     def __post_init__(self):
         self._dim_names = {}
+        if self.stats:
+            self.stat_data = {}
 
     def set_dim_name(self, dim: Iterable[int], name: str):
         self._dim_names[dim] = name
 
     def update_dim(self, dim: Iterable[int], value):
-        self.collection_algorithm(dim, value)
+        self.collector.update_dim(dim, value)
 
-    def export_dims(self):
+    def update_done(self):
+        self.collector.update_done()
+
+        if self.stats:
+            std_axis = len(self.collector.shape) - 1
+            for stat in self.stats:
+                self.stat_data[stat] = stat(self.collector.raw_data, axis=std_axis)
+
+    def export_stats(self):
         dims = [range(i) for i in self.shape]
 
         for dim in itertools.product(*dims):
-            name = self._dim_names[dim]
-            value = self.collection_algorithm.data[dim]
+            if self.stats:
+                for stat in self.stats:
+                    name = self._dim_names[dim] + '_' + stat.__name__
+                    value = self.stat_data[stat][dim]
 
-            yield Dim(name=name, value=value)
+                    yield Dim(name=name, value=value)
+            else:
+                name = self._dim_names[dim]
+                value = self.collector.raw_data[dim]
+
+                yield Dim(name=name, value=value)
 
 
 @dataclass
@@ -41,34 +62,34 @@ class Latest(object):
     shape: Iterable[int]
 
     def __post_init__(self):
-        self.data = np.zeros(self.shape)
+        self.raw_data = np.zeros(self.shape)
 
-    def __call__(self, dim: Iterable[int], value):
-        self.data[dim] = value
+    def update_dim(self, dim: Iterable[int], value):
+        self.raw_data[dim] = value
+
+    def update_done(self):
+        pass
 
 
 @dataclass
 class SlidingWindow(object):
     shape: Iterable[int]
     window_size: int
-    dim_calc: Callable
 
     def __post_init__(self):
         shape_with_window = list(self.shape)
         shape_with_window.append(self.window_size)
 
-        self._raw_data = np.zeros(shape_with_window)
-        self.data = np.ndarray(self.shape)
+        self.raw_data = np.zeros(shape_with_window)
 
-        self._current_sample_index = 0
+        self._sample_index = 0
 
-    def __call__(self, dim: Iterable[int], value):
+    def update_dim(self, dim: Iterable[int], value):
         shape_index = list(dim)
-        shape_index.append(self._current_sample_index)
+        shape_index.append(self._sample_index)
         shape_index = tuple(shape_index)
 
-        self._raw_data[shape_index] = value
+        self.raw_data[shape_index] = value
 
-        self._current_sample_index = (self._current_sample_index + 1) % self.window_size
-
-        self.data[dim] = self.dim_calc(self._raw_data[dim])
+    def update_done(self):
+        self._sample_index = (self._sample_index + 1) % self.window_size

metrics/softirqs.py
@@ -1,12 +1,13 @@
 import asyncio
 import logging
 import os
-from dataclasses import dataclass, field
-from typing import Any, List
+from dataclasses import dataclass
+from typing import List
+
 import numpy as np
 from bcc import BPF
 
-from metrics.metric import Metric, SlidingWindow, Latest
+from metrics.metric import Latest, Metric, SlidingWindow
 
 _bpf_softirq_limit = """
 #include <uapi/linux/ptrace.h>
@@ -125,51 +126,37 @@ class SoftIRQs:
     std_factor: float = 3
     limits: Limits = None
 
-    _bpf_handler: Any = field(default=None, init=False)
-
-    _can_run: asyncio.Event = field(default=asyncio.Event(), init=False)
-
-    _cpu_count = os.cpu_count()
-    _shape = (_cpu_count, len(_SOFT_IRQS))
-
     def __post_init__(self):
+        self._cpu_count = os.cpu_count()
+        self._shape = (self._cpu_count, len(_SOFT_IRQS))
+
         self._define_metrics()
 
-        if self.limits is not None:
-            self._bpf_handler = BPF(text=_bpf_softirq_limit)
-            self._update_limits()
-            self._bpf_handler['alerts'].open_perf_buffer(self._handle_alert)
-        else:
-            self.update_limits(
-                Limits(timer_irq_per_sec=[-1] * self._cpu_count)
-            )
-
+        self._can_run: asyncio.Event = asyncio.Event()
         self._can_run.set()
+
+        self._bpf_handler: BPF = BPF(text=_bpf_softirq_limit)
+        self.limits = self.limits or Limits(timer_irq_per_sec=[-1] * self._cpu_count)
+        self._update_limits()
+        self._bpf_handler['alerts'].open_perf_buffer(self._handle_alert)
 
     def _define_metrics(self):
         class_name = self.__class__.__name__
 
         LOGGER.info(f'Defining metric: {class_name}')
-        self._metric = Metric(
-            name=f'{class_name}Metric',
-            shape=self._shape,
-            collection_algorithm=SlidingWindow(
-                shape=self._shape,
-                window_size=self.sliding_window_size,
-                dim_calc=np.std
-            )
-        )
-
         LOGGER.info(f'Defining alert: {class_name}')
-        self._alert = Metric(
-            name=f'{class_name}Alert',
-            shape=self._shape,
-            collection_algorithm=Latest(shape=self._shape)
-        )
+        self._metric = Metric(name=class_name + 'Metric',
+                              shape=self._shape,
+                              stats=[np.mean, np.std],
+                              collector=SlidingWindow(shape=self._shape,
+                                                      window_size=self.sliding_window_size))
+        self._alert = Metric(name=class_name + 'Alert',
+                             shape=self._shape,
+                             collector=Latest(shape=self._shape))
 
         for core in range(self._shape[0]):
             for vec in range(self._shape[1]):
-                metric_name = f'cpu_{core}_irq_{_vec_to_name(vec)}_std'
+                metric_name = f'cpu_{core}_irq_{_vec_to_name(vec)}'
                 alert_name = f'cpu_{core}_irq_{_vec_to_name(vec)}_alert'
 
                 LOGGER.info(f'Dimension: {metric_name} Alert: {alert_name}')
@@ -181,8 +168,11 @@ class SoftIRQs:
 
     def _save_histogram(self):
         dist_cpu = self._bpf_handler.get_table("dist_cpu")
+
         for k, v in dist_cpu.items():
             self._metric.update_dim((k.cpu, k.vec), v.value)
+
+        self._metric.update_done()
 
         dist_cpu.clear()
 
@@ -190,16 +180,12 @@ class SoftIRQs:
         alert = self._bpf_handler['alerts'].event(data)
         self._alert.update_dim((core, alert.vec), alert.timestamp)
 
-    def update_limits(self, limits: Limits):
-        self.limits = limits
-        self._update_limits()
-
     def _update_limits(self):
         ulimits = self._bpf_handler['user_limits']
         limits_val = ulimits[0]
 
         for i in range(len(self.limits.timer_irq_per_sec)):
-            limits_val.timer_irq_per_second[i] =\
+            limits_val.timer_irq_per_second[i] = \
                 self.limits.timer_irq_per_sec[i]
 
         ulimits.update({0: limits_val})